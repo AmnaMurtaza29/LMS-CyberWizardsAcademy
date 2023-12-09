2bf734e185d610d8355df04c6d7098a7473b8bf6
Amna Sadia Korai <murtazaamna117@gmail.com>
2023-12-09 19:08:15 +0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php
+include('./header.php')
+?>
+<div class="container-fluid" id="Contact">
+  <div class="row contact-section">
+    <div class="col-lg-6 col-md-12 contact-form">
+      <h2>Contact Us</h2>
+      <form action="" method="post">
+        <input type="text" class="form-control" name="name" placeholder="Your Name" required><br>
+        <input type="text" class="form-control" name="subject" placeholder="Subject" required><br>
+        <input type="email" class="form-control" name="email" placeholder="Your Email" required><br>
+        <textarea class="form-control" name="message" placeholder="How can we assist you?" rows="5" required></textarea><br>
+        <button type="submit" class="btn-btn-danger">Send Message</button>
+      </form>
+    </div> <br>
+    <div class="col-lg-6 col-md-12 contact-info">
+      <h2>Get in Touch</h2>
+      <p>If you have any questions, feel free to reach out to us. We are here to help!</p>
+      <ul>
+        <li><strong>Address:</strong> CyberWizards Academy Pvt Ltd, Gulshan Iqbal, Karachi</li>
+        <li><strong>Phone:</strong> 012 345678</li>
+        <li><strong>Email:</strong> info@cyberwizards.com</li>
+      </ul>
+      <div class="social-icons">
+        <a href="#" class="social-icon"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="social-icon"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="social-icon"><i class="fab fa-whatsapp"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+<?php
+include('./footer.php')
+?>
+
+
+
+
+
+
+
+<!-- Start Social Follow- -->
+<!-- Start Footer -->
+
+<!-- End Social Follow -->
+<!-- End Footer -->
---
+++ courses.php
@@ -0,0 +1,181 @@
+<?php
+include('./header.php')
+?>
+<!--Start Background -->
+<div class="container-fluid remove-vid-marg">
+    <div class="vid-parent">
+        <video playsinline autoplay muted loop>
+            <source src="videos/hackbanner.mp4">
+        </video>
+        <div class="vid-overlay"></div>
+    </div>
+    <div class="vid-content">
+    <h1 class="my-content">Uncover the Gateway to Knowledge.</h1>
+    <small class="my-content">Elevate Your Skills in the Digital Realm: Join Cyber Wizards for a Journey Beyond Boundaries.</small> 
+    <br>
+    <br>
+    <a href="#" class="btn-btn-danger bg-rgb(53, 18, 53)" data-bs-toggle="modal" data-bs-target="#LoginModal">Begin Your Journey</a>
+    </div>
+    
+</div>
+
+<!--End Background-->
+
+
+<!--Start Text Banner-->
+
+<div class="container-fluid txt-banner">
+  <div class="row bottom-banner">
+    <div class="col-sm">
+      <h5><i class="fas fa-book-open mr-3"></i> 50+ Online Courses </h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-users mr-3"></i> Proficient Educators</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-keyboard mr-3"></i> Lifetime Access</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-rupee-sign mr-3"></i> Money Back Gurantee</h5>
+    </div>
+  </div>
+</div>
+
+
+<!-- End Text Banner-->
+
+
+<!-- Start Popular Courses -->
+
+<div class="container mt-5">
+    <h1 class="text-center">Popular Courses</h1><br><br>
+    <div class="row">
+
+        <!-- Course 1 -->
+        <div class="col-md-4">
+            <div class="card">
+                <img src="Images/cc.jpg" class="card-img-top" alt="Course Image">
+                <div class="card-body">
+                    <h5 class="card-title">CyberCraft Mastery Series</h5>
+                    <p class="card-text">A comprehensive hacking course designed to take you from a novice to an advanced cybersecurity expert...</p>
+                </div>
+                <div class="card-footer">
+                    <p class="card-text d-inline">Price: <span class="font-weight-bolder">20000 PKR</span></p>
+                    <a class="btn-btn-danger btn-outline" href="#">Enroll Now</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Course 2 -->
+        <div class="col-md-4">
+            <div class="card">
+                <img src="Images/linux.jpg" class="card-img-top" alt="Course Image">
+                <div class="card-body">
+                    <h5 class="card-title">Linux Mastery and Kali Linux Security</h5>
+                    <p class="card-text">Whether you're a beginner or an experienced user, this comprehensive program will empower you with the skills to navigate the Linux ecosystem fluently and...</p>
+                </div>
+                <div class="card-footer">
+                    <p class="card-text d-inline">Price: <span class="font-weight-bolder">15000 PKR</span></p>
+                    <a class="btn-btn-danger btn-outline" href="#">Enroll Now</a>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+
+        <!-- Course 3 -->
+        <div class="col-md-4">
+            <div class="card">
+                <img src="Images/python.jpg" class="card-img-top" alt="Course Image">
+                <div class="card-body">
+                    <h5 class="card-title">Python for Offensive Cybersecurity</h5>
+                    <p class="card-text">Unleash the power of Python in the realm of offensive cybersecurity with our cutting-edge course, Python for Offensive Cybersecurity...</p>
+                </div>
+                <div class="card-footer">
+                    <p class="card-text d-inline">Price: <span class="font-weight-bolder">25000 PKR</span></p>
+                    <a class="btn-btn-danger btn-outline" href="#">Enroll Now</a>
+                </div>
+            </div>
+        </div>
+        <br><br><br>
+
+        <!-- Continued HTML for Course 4 -->
+<div class="col-md-4">
+    <div class="card">
+        <img src="Images/bugg.jpg" class="card-img-top" alt="Course Image">
+        <div class="card-body">
+            <h5 class="card-title">BountyCraft: Mastering the Art of Bug Hunting</h5>
+            <p class="card-text">Embark on a dynamic journey into the world of ethical hacking and cybersecurity with our cutting-edge course, BountyCraft: Mastering the Art of Bug Hunting...</p>
+        </div>
+        <div class="card-footer">
+            <p class="card-text d-inline">Price: <span class="font-weight-bolder">10000 PKR</span></p>
+            <a class="btn-btn-danger btn-outline" href="#">Enroll Now</a>
+        </div>
+    </div>
+</div>
+
+<!-- Continued HTML for Course 5 -->
+<div class="col-md-4">
+    <div class="card">
+        <img src="Images/offense.jpg" class="card-img-top" alt="Course Image">
+        <div class="card-body">
+            <h5 class="card-title">Advanced Offensive Security Tactics</h5>
+            <p class="card-text">Dive into the intricacies of offensive security with our Advanced Offensive Security Tactics course. Geared towards cybersecurity professionals, ethical hackers, and penetration testers...</p>
+        </div>
+        <div class="card-footer">
+            <p class="card-text d-inline">Price: <span class="font-weight-bolder">20000 PKR</span></p>
+            <a class="btn-btn-danger btn-outline" href="#">Enroll Now</a>
+        </div>
+    </div>
+</div>
+
+<!-- Continued HTML for Course 6 -->
+<div class="col-md-4">
+    <div class="card">
+        <img src="Images/defense.jpg" class="card-img-top" alt="Course Image">
+        <div class="card-body">
+            <h5 class="card-title">GuardianShield: Mastering Defensive Cybersecurity</h5>
+            <p class="card-text">This comprehensive program empowers you with the knowledge and skills to fortify systems, protect sensitive data, and proactively defend against evolving cyber threats...</p>
+        </div>
+        <div class="card-footer">
+            <p class="card-text d-inline">Price: <span class="font-weight-bolder">15000 PKR</span></p>
+            <a class="btn-btn-danger btn-outline" href="#">Enroll Now</a>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+
+  <!-- End Most Popular Course 1st Card Deck -->
+</div>
+<div class="text-center m-2">
+    <a class="btn-btn-danger" href="#">View All Courses</a>
+    <br>
+  </div>
+</div> <br><br>
+
+<!-- End Popular Courses -->
+
+
+
+
+
+
+
+
+
+<?php
+include('./footer.php')
+?>
+
+
+
+
+
+
+
+<!-- Start Social Follow- -->
+<!-- Start Footer -->
+
+<!-- End Social Follow -->
+<!-- End Footer -->
